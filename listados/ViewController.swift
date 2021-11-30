21fe9092f9a9606f4a48f12b79b1b66fdedd25e5
Luis <luismartinezmilla@gmail.com>
2021-11-30 12:01:52 +0100

    lista inicial funcional

--- listados/ViewController.swift
+++ listados/ViewController.swift
@@ -7,13 +7,31 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    @IBOutlet weak var tabla: UITableView!
+    let numeros = [1,2,3,4,5,6,7]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        tabla.dataSource = self
+        tabla.delegate = self
     }
-
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return numeros.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let celda = tableView.dequeueReusableCell(withIdentifier: "celdaID", for: indexPath)
+        celda.textLabel?.text = String(numeros[indexPath.row])
+        return celda
+    }
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("has seleccionado la fila del indice", indexPath.row)
+    }
+    
 
 }
 
